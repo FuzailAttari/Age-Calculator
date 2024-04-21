20265356930c8b66ec265d4006fa49e4c18f4330
Fuzail Raza Attari (Manai) <hunailattari@gmail.com>
2024-04-21 21:25:21 +0500

    codding

---
+++ app.js
@@ -0,0 +1,9 @@
+document.write(`<Center> <h1> Age Calculator </h1>`)
+
+
+var userinpu= prompt("Enter Your DOB We Show Your Current Age Enter this Formated Example 12 Dec,1991 ")
+var getdate = new Date().getTime()
+var dob = new Date(userinpu).getTime()
+var mius= getdate-dob
+var formula = mius / (1000*60*60*24*365)
+document.write( ` <h1> Your Current Age is  ` + Math.floor(formula))
